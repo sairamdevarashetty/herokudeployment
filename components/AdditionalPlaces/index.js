5957ffe50e7e7af711015ee8b4f81dfe94e00089
devarashettysairam <devarashetty.sairam@jda.com>
2020-02-24 23:08:44 +0530

    added changes

--- components/AdditionalPlaces/index.js
+++ components/AdditionalPlaces/index.js
@@ -26,14 +26,16 @@ export default function AdditionalPlaces ({ imagesList = [] }) {
                         <h3><font style={{verticalAlign: 'inherit'}}><font style={{verticalAlign: 'inherit'}}>
                             Visit every corner of our lake.
                             </font></font></h3>
-                        <p><font style={{verticalAlign: 'inherit'}}><font style={{verticalAlign: 'inherit'}}>It is the only one, among the pre-Alpine ones, which expands in the high plain, changing its appearance: from a physiognomy of a valley lake, with the characteristics of a real fjord, similar to the other subalpine lakes, to that of a small plain sea. </font><font style={{verticalAlign: 'inherit'}}>Those who come from northern Europe along the Lagarina valley, arrive in Torbole and Riva del Garda and have the feeling of being in front of a landscape very similar to a Nordic fjord, dominated on the eastern side by Monte Baldo.</font></font></p>
+                        <p><font style={{verticalAlign: 'inherit'}}><font style={{verticalAlign: 'inherit'}}>
+                            It is the only one, among the pre-Alpine ones, which expands in the high plain, changing its appearance: from a physiognomy of a valley lake, with the characteristics of a real fjord, similar to the other subalpine lakes, to that of a small plain sea. </font><font style={{verticalAlign: 'inherit'}}>Those who come from northern Europe along the Lagarina valley, arrive in Torbole and Riva del Garda and have the feeling of being in front of a landscape very similar to a Nordic fjord, dominated on the eastern side by Monte Baldo.
+                        </font></font></p>
                     </div>
                 </div>
                 <div className="other-places-grid">
                     <div className="row">
                         {
-                            imagesList.map(({ url, text }) => (
-                                <CardComponent url={url} text={text} />
+                            imagesList.map(({ url, nome }) => (
+                                <CardComponent url={url} text={nome} />
                             ))
                         }
                     </div>
